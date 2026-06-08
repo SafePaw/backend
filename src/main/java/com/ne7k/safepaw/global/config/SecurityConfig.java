d8e7f46040e56e10740c469bf87562b8200cce8b
NE7K <sjjang16@naver.com>
2026-06-08 14:19:38 +0900

    Feat : Added Cors port 5173

--- src/main/java/com/ne7k/safepaw/global/config/SecurityConfig.java
+++ src/main/java/com/ne7k/safepaw/global/config/SecurityConfig.java
@@ -15,6 +15,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -27,6 +32,7 @@ public class SecurityConfig {
             ) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // jwt 사용으로 인한 csrf 차단
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // cors
                 .sessionManagement(s -> s // 세션 미사용
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth // 검사 제외 url
@@ -66,5 +72,22 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // localhost 5173
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173"
+                //"https://safepaw.vercel.app"   // 추후 실제 Vercel URL로 교체
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", config);
+        return source;
     }
 }
